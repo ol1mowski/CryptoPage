fc765e8ca139471cfded570fbc0c7eed39472cef
Oliwier <101707516+ol1mowski@users.noreply.github.com>
2024-09-01 14:19:15 +0200

    adding bem classes

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import s from "./Home.module.scss";
+
+import image from '../../assets/mainImages.png'
 
 function Home() {
   return (
-    <section>
-        
+    <section className={s.container}>
+      <section className={s.container__ctaInfo}>
+        <h1 className={s.container__ctaInfo__title}>Crypto Charts</h1>
+        <p className={s.container__ctaInfo__desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+          voluptatibus quos velit libero enim nam consectetur facere tempore
+          aliquid? Quaerat tempore aperiam blanditiis magnam quidem alias cum
+          fuga delectus iusto.
+        </p>
+        <button className={s.container__ctaInfo__btn}>Check It Now</button>
+      </section>
+      <section className={s.container__imageSide}>
+        <img className={s.container__imageSide__img} src={image} alt="home page image" />
+      </section>
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
