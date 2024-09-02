fb8b91935de4b31b4058d75fea39864fecfdbda2
Oliwier <101707516+ol1mowski@users.noreply.github.com>
2024-09-02 15:34:35 +0200

    add style to the price section component

--- src/components/PriceSection/PriceSection.jsx
+++ src/components/PriceSection/PriceSection.jsx
@@ -1,18 +1,46 @@
-import s from './PriceSection.module.scss';
+import s from "./PriceSection.module.scss";
 
-import btc from '../../assets/btc.svg'
-import eth from '../../assets/eth.svg'
-import ltc from '../../assets/ltc.svg'
-import doge from '../../assets/doge.svg'
+import btc from "../../assets/btc.svg";
+import eth from "../../assets/eth.svg";
+import ltc from "../../assets/ltc.svg";
+import doge from "../../assets/doge.svg";
 
 function PriceSection() {
   return (
-    <section>
-      <h1>Cryptocurrency Prices</h1>
-      <section>
-        <div>
-          <img src={btc} alt="btc icon" />
-          <span>34</span>
+    <section className={s.container}>
+      <h1 className={s.container__header}>
+        Cryptocurrency <span className={s.container__header__span}>Prices</span>
+      </h1>
+      <section className={s.container__iconsWrapper}>
+        <div className={s.container__iconsWrapper__iconBox}>
+          <img
+            className={s.container__iconsWrapper__iconBox__img}
+            src={btc}
+            alt="btc icon"
+          />
+          <span className={s.container__iconsWrapper__iconBox__price}>
+            $34.653
+          </span>
+        </div>{" "}
+        <div className={s.container__iconsWrapper__iconBox}>
+          <img
+            className={s.container__iconsWrapper__iconBox__img}
+            src={btc}
+            alt="btc icon"
+          />
+          <span className={s.container__iconsWrapper__iconBox__price}>
+            $34.653
+          </span>
+        </div>{" "}
+        <div className={s.container__iconsWrapper__iconBox}>
+          <img
+            className={s.container__iconsWrapper__iconBox__img}
+            src={btc}
+            alt="btc icon"
+          />
+          <span className={s.container__iconsWrapper__iconBox__price}>
+            $34.653
+          </span>
         </div>
       </section>
     </section>
